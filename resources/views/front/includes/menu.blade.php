d14e317e1f5d58b2ddb337a035234fb3414caac1
Hycinthe ABANDA <hyacinthabanda@gmail.com>
2021-11-17 00:35:12 +0100

    add icon on menu

--- resources/views/front/includes/menu.blade.php
+++ resources/views/front/includes/menu.blade.php
@@ -1,41 +1,41 @@
 <nav class="menu-navbar">
   <ul>
-    <li class="deroulant"><a href="#">LES FONDAMENTAUX</a>
+    <li class="deroulant"><a href="#"><i class="feather icon-home"></i> LES FONDAMENTAUX</a>
       <ul class="sous">
         <li><a href="#">L'histoire</a></li>
         <li><a href="#">Les guérisons et miracles</a></li>
         <li><a href="#">Prières</a></li>
       </ul>
     </li>
-    <li class="deroulant"><a href="#">ACTIVITÉS DU PÈLERIN</a>
+    <li class="deroulant"><a href="#"><i class="feather icon-activity"></i> ACTIVITÉS DU PÈLERIN</a>
       <ul class="sous">
         <li><a href="#">CSS display</a></li>
         <li><a href="#">CSS position</a></li>
         <li><a href="#">CSS float</a></li>
       </ul>
     </li>
-    <li class="deroulant"><a href="#">VENIR AU SANCTUAIRE</a>
+    <li class="deroulant"><a href="#"><i class="feather icon-briefcase"></i> VENIR AU SANCTUAIRE</a>
       <ul class="sous">
         <li><a href="#">CSS display</a></li>
         <li><a href="#">CSS position</a></li>
         <li><a href="#">CSS float</a></li>
       </ul>
     </li>
-    <li class="deroulant"><a href="#">ACTUALITES</a>
+    <li class="deroulant"><a href="#"><i class="feather icon-book"></i> ACTUALITES</a>
       <ul class="sous">
         <li><a href="#">CSS display</a></li>
         <li><a href="#">CSS position</a></li>
         <li><a href="#">CSS float</a></li>
       </ul>
     </li>
-    <li class="deroulant"><a href="#">SERVIR AU SANCTUAIRE</a>
+    <li class="deroulant"><a href="#"> <i class="feather icon-users"></i> SERVIR AU SANCTUAIRE</a>
       <ul class="sous">
         <li><a href="#">CSS display</a></li>
         <li><a href="#">CSS position</a></li>
         <li><a href="#">CSS float</a></li>
       </ul>
     </li>
-    <li class="deroulant"><a href="#">TV SANCTUAIRE</a>
+    <li class="deroulant"><a href="#"><i class="feather icon-tv"></i> TV SANCTUAIRE</a>
       <ul class="sous">
         <li><a href="#">Direct</a></li>
         <li><a href="#">Replay</a></li>
