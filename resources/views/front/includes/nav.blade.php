82bde6a0f3651d4868ed8b4d2f17c9aa54e9359f
Hycinthe ABANDA <hyacinthabanda@gmail.com>
2021-11-17 01:55:42 +0100

    add logo

--- resources/views/front/includes/nav.blade.php
+++ resources/views/front/includes/nav.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row">
       <div class="col-sm-3">
-        <img src="{{ asset('/assets/img/logos/caa-primary.png') }}" class="img-responsive" alt="CAA Sydney logo">
+        <img src="{{ asset('/assets/img/logos/logo-sanctuaire.png') }}" class="img-responsive" alt="CAA Sydney logo">
       </div>
       <div class="col-sm-9">
           <div class="items">
