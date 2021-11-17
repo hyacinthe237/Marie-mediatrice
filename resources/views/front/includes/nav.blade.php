398f30dd8f93720e180f7a6306aa828f02fd9db1
Hycinthe ABANDA <hyacinthabanda@gmail.com>
2021-11-17 01:08:54 +0100

    menu du milieu

--- resources/views/front/includes/nav.blade.php
+++ resources/views/front/includes/nav.blade.php
@@ -1,46 +1,19 @@
-<nav class="navbar navbar-default"  id="navbar">
-    <div class="container">
-        <div class="navbar-header">
-            {{-- <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="feather icon-menu"></span>
-            </button> --}}
-            <a class="navbar-brand" href="/">
-                <img src="{{ asset('/assets/img/logos/caa-primary.png') }}" alt="CAA Sydney logo">
-            </a>
-        </div>
-        {{-- <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <a href="{{ route('home') }}">
-                        News
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        Activities
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        Membership
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        Constitution
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        Contact
-                    </a>
-                </li>
-            </ul>
-        </div> --}}
+<div class="container">
+    <div class="row">
+      <div class="col-sm-3">
+        <img src="{{ asset('/assets/img/logos/caa-primary.png') }}" class="img-responsive" alt="CAA Sydney logo">
+      </div>
+      <div class="col-sm-9">
+          <div class="items">
+              <div class="item">s'identifier</div>
+              <div class="item bg">Offrande de messe</div>
+              <div class="item bg">faire un don</div>
+              <div class="item bg">Charte sanitaire</div>
+              <div class="item">Incription Newsletter</div>
+              <div class="item bg">déposer un cierge</div>
+              <div class="item bg">intention de prière</div>
+              <div class="item bg">déposer une couronne</div>
+          </div>
+      </div>
     </div>
-</nav>
-
-@if ($fixed) 
-<div class="navbar-clearfix"></div>
-@endif
+</div>
