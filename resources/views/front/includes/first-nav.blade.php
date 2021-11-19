c90dc3a6d39cc24f7217f3e0c00b3d9b91a5436f
Hycinthe ABANDA <hyacinthabanda@gmail.com>
2021-11-19 09:11:59 +0100

    start first nav

--- resources/views/front/includes/first-nav.blade.php
+++ resources/views/front/includes/first-nav.blade.php
@@ -1,1 +1,18 @@
-
+<div class="first-menu">
+  <div class="icons">
+    <ul>
+      <li>F</li>
+      <li>T</li>
+      <li>I</li>
+      <li>T</li>
+      <li>Z</li>
+      <li>Y</li>
+    </ul>
+  </div>
+  <div class="lang">
+    langue
+  </div>
+  <div class="search">
+    search
+  </div>
+</div>
